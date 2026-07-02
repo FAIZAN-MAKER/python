a45d9a59e4086ec0d7ba4f871c47afbf9340432a
Muhammad-Faizan <un6902090@gmail.com>
2026-07-02 19:00:44 +0500

    Add initial implementation of questions for basic Python exercises

---
+++ day2/questions.py
@@ -0,0 +1,28 @@
+# Q:1 Print Hello world n times.
+
+# n = int(input("Enter a number"))
+
+# for i in range(1, n+1):
+#     print("Hello World!")
+    
+# Q:2 Print natural number from 1 to n
+
+# n = int(input("Enter a number"))
+
+# for i in range(1, n+1):
+#     print(i)
+    
+    
+# Q:3 Reverse loop.
+
+# n = int(input("Enter a number: "))
+
+# for i in range(n, 0, -1):
+#     print(i)
+    
+# Q:4 Print the multiplication table of a number
+
+# n = int(input("Enter a number: "))
+
+# for i in range(1, 11):
+#     print(f"{n} x {i} = {n*i}")
